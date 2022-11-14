c6ca2040ff7cce5982c512093f402bff2e985f49
Biljana Novakovic <novakovic1701@gmail.com>
2022-11-14 14:26:22 +0100

    Dodata baza

---
+++ dbBroker.php
@@ -0,0 +1,21 @@
+<?php
+
+$host = "localhost";
+$db = "tretmani";
+$user = "root";
+$pass = "";
+
+$conn = new mysqli($host,$user,$pass,$db);
+
+
+if ($conn->connect_errno){
+    exit("Nauspesna konekcija: greska> ".$conn->connect_error.", err kod>".$conn->connect_errno);
+}
+
+
+
+
+
+
+
+?>

20f36d255d861c8215993b6c5282cb2b4abf2aa6
Biljana Novakovic <novakovic1701@gmail.com>
2023-06-11 20:01:51 +0200

    napravljena konekcija sa bazom

---
+++ konekcija.php
@@ -0,0 +1,9 @@
+<?php
+  error_reporting(E_ALL | E_STRICT);
+  ini_set("display_errors", true);
+  ini_set("log_errors", true);
+
+
+  $conn = new MySqli('localhost', 'root','','cosmetics');
+  $conn->set_charset("utf8");
+ ?>
